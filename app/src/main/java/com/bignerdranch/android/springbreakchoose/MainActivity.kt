fdd491a1510b91f2334c238b4c5f86e19ad85326
ttaliacc <taliac@bu.edu>
2024-03-14 14:22:38 -0700

    implement the audio record

--- app/src/main/java/com/bignerdranch/android/springbreakchoose/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/springbreakchoose/MainActivity.kt
@@ -7,15 +7,24 @@ import androidx.core.app.ActivityCompat
 import com.bignerdranch.android.springbreakchoose.databinding.ActivityMainBinding
 import com.google.mlkit.common.model.DownloadConditions
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.media.MediaRecorder
+import android.speech.RecognizerIntent
+import android.widget.EditText
+import android.widget.Toast
+import java.util.Locale
+import java.util.Objects
 
 const val REQUEST_CODE = 200
 class MainActivity : AppCompatActivity() {
-
+    lateinit var editText: EditText
     private lateinit var binding: ActivityMainBinding
     private var permissions = arrayOf(Manifest.permission.RECORD_AUDIO)
     private var permissionGranted = false
+    private lateinit var recorder: MediaRecorder
     private var conditions = DownloadConditions.Builder().requireWifi().build()
+    private val REQUEST_CODE_SPEECH_INPUT = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +34,7 @@ class MainActivity : AppCompatActivity() {
         permissionGranted= ActivityCompat.checkSelfPermission(this, permissions[0]) == PackageManager.PERMISSION_GRANTED
         if(!permissionGranted)
             ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE)
+        editText = findViewById(R.id.editText)
 
         val spinner = binding.spinner
         val record = binding.recordBtn
@@ -35,7 +45,9 @@ class MainActivity : AppCompatActivity() {
 
         spinner.adapter = adapter
 
-        record.setOnClickListener {  }
+        record.setOnClickListener {
+            startRecording()
+        }
     }
 
     override fun onRequestPermissionsResult(
@@ -55,5 +67,49 @@ class MainActivity : AppCompatActivity() {
             return
         }
         // start recording
+        // on below line we are calling speech recognizer intent.
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        intent.putExtra(
+            RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+        )
+        intent.putExtra(
+            RecognizerIntent.EXTRA_LANGUAGE,
+            Locale.getDefault()
+        )
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak to text")
+        try {
+            startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT)
+        } catch (e: Exception) {
+            // on below line we are displaying error message in toast
+            Toast
+                .makeText(
+                    this@MainActivity, " " + e.message,
+                    Toast.LENGTH_SHORT
+                )
+                .show()
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // in this method we are checking request
+        // code with our result code.
+        if (requestCode == REQUEST_CODE_SPEECH_INPUT) {
+            // on below line we are checking if result code is ok
+            if (resultCode == RESULT_OK && data != null) {
+
+                // in that case we are extracting the
+                // data from our array list
+                val res: ArrayList<String> =
+                    data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS) as ArrayList<String>
+
+                // on below line we are setting data
+                // to our output text view.
+                editText.setText(
+                    Objects.requireNonNull(res)[0]
+                )
+            }
+        }
     }
 }
