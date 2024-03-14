81392d9392a5089d531aa0b3b1bf3aacdca7c1c2
ttaliacc <taliac@bu.edu>
2024-03-14 11:51:15 -0700

    complete ui

--- app/src/main/java/com/bignerdranch/android/springbreakchoose/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/springbreakchoose/MainActivity.kt
@@ -2,10 +2,24 @@ package com.bignerdranch.android.springbreakchoose
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.bignerdranch.android.springbreakchoose.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val spinner = binding.spinner
+
+        val languages = resources.getStringArray(R.array.languages)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, languages)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spinner.adapter = adapter
     }
 }
