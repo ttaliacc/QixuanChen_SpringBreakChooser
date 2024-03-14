8a002bc8c622ec7a4e6cbab3ac7ee5f702980e13
ttaliacc <taliac@bu.edu>
2024-03-14 12:56:06 -0700

    complete audio permission

--- app/src/main/java/com/bignerdranch/android/springbreakchoose/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/springbreakchoose/MainActivity.kt
@@ -3,23 +3,57 @@ package com.bignerdranch.android.springbreakchoose
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import androidx.core.app.ActivityCompat
 import com.bignerdranch.android.springbreakchoose.databinding.ActivityMainBinding
+import com.google.mlkit.common.model.DownloadConditions
+import android.Manifest
+import android.content.pm.PackageManager
 
+const val REQUEST_CODE = 200
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var permissions = arrayOf(Manifest.permission.RECORD_AUDIO)
+    private var permissionGranted = false
+    private var conditions = DownloadConditions.Builder().requireWifi().build()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        permissionGranted= ActivityCompat.checkSelfPermission(this, permissions[0]) == PackageManager.PERMISSION_GRANTED
+        if(!permissionGranted)
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE)
+
         val spinner = binding.spinner
+        val record = binding.recordBtn
 
         val languages = resources.getStringArray(R.array.languages)
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, languages)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
         spinner.adapter = adapter
+
+        record.setOnClickListener {  }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(requestCode == REQUEST_CODE)
+            permissionGranted = grantResults[0] == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun startRecording() {
+        if(!permissionGranted){
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE)
+            return
+        }
+        // start recording
     }
 }
